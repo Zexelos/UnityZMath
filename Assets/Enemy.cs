a9b9f5f20c3e61157c78f12929135f09cd0eaf2c
DESKTOP-HJMTSJF\Zexelos <zexelosg@gmail.com>
2021-02-09 20:35:10 +0100

    player chasing

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,5 +2,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Transform player;
+    [SerializeField] float fov;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float sightRange;
     public float radius = 1f;
+
+    float oneSideFov;
+
+    private void Start()
+    {
+        oneSideFov = fov / 2;
+    }
+
+    void Update()
+    {
+        Vector3 playerDir = player.position - transform.position;
+        float distance = Mathf.Sqrt(playerDir.x * playerDir.x + playerDir.y * playerDir.y + playerDir.z * playerDir.z);
+        Vector3 playerDirN = playerDir / distance;
+        float dotForward = playerDirN.x * transform.forward.x + playerDirN.y * transform.forward.y + playerDirN.z * transform.forward.z;
+        float angle = Mathf.Acos(dotForward) * Mathf.Rad2Deg;
+
+        if (angle < oneSideFov && distance < sightRange)
+        {
+            Debug.Log("Chasing");
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            //transform.rotation = Quaternion.Euler(0, angle - 90, 0);
+        }
+    }
 }
