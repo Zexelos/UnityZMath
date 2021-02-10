77eec01ffba78728c25bd765d716b6e8452e719a
DESKTOP-HJMTSJF\Zexelos <zexelosg@gmail.com>
2021-02-10 20:13:06 +0100

    added patrolling and attacking

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,33 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum State
+{
+    Roam,
+    Chase
+}
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform player;
+    public float radius = 1f;
+    [SerializeField] Player player;
     [SerializeField] float fov;
     [SerializeField] float moveSpeed;
     [SerializeField] float sightRange;
-    public float radius = 1f;
+    [SerializeField] int maxHP;
+    [SerializeField] List<Transform> path;
 
     float oneSideFov;
+    Transform currentTarget;
+    Transform currentPathTarget;
+    Vector3 currentTargetDirection;
+    float currentTargetDistance;
+    State currentState;
+    bool canAttack;
+    int currentHP;
 
-    private void Start()
+    void Start()
     {
         oneSideFov = fov / 2;
+        currentPathTarget = path[Random.Range(0, path.Count)];
+        currentTarget = currentPathTarget;
+        currentState = State.Roam;
+        canAttack = true;
+        currentHP = maxHP;
     }
 
     void Update()
     {
-        Vector3 playerDir = player.position - transform.position;
-        float distance = Mathf.Sqrt(playerDir.x * playerDir.x + playerDir.y * playerDir.y + playerDir.z * playerDir.z);
-        Vector3 playerDirN = playerDir / distance;
+        Vector3 playerDir = player.transform.position - transform.position;
+        float playerDistance = Mathf.Sqrt(playerDir.x * playerDir.x + playerDir.y * playerDir.y + playerDir.z * playerDir.z);
+        Vector3 playerDirN = playerDir / playerDistance;
         float dotForward = playerDirN.x * transform.forward.x + playerDirN.y * transform.forward.y + playerDirN.z * transform.forward.z;
-        float angle = Mathf.Acos(dotForward) * Mathf.Rad2Deg;
+        float playerAngle = Mathf.Acos(dotForward) * Mathf.Rad2Deg;
 
-        if (angle < oneSideFov && distance < sightRange)
+        switch (currentState)
         {
-            Debug.Log("Chasing");
-            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-            transform.rotation = Quaternion.LookRotation(playerDir);
+            case State.Roam:
+                currentTarget = currentPathTarget;
+
+                currentTargetDirection = currentTarget.position - transform.position;
+                currentTargetDistance = Mathf.Sqrt(currentTargetDirection.x * currentTargetDirection.x + currentTargetDirection.y * currentTargetDirection.y + currentTargetDirection.z * currentTargetDirection.z);
+
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentTarget.position.x, transform.position.y, currentTarget.position.z), moveSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.LookRotation(new Vector3(currentTargetDirection.x, 0, currentTargetDirection.z));
+
+                if (currentTargetDistance < 2f)
+                {
+                    currentPathTarget = path[Random.Range(0, path.Count)];
+                    currentTarget = currentPathTarget;
+                }
+
+                if (playerDistance <= sightRange && playerAngle <= oneSideFov)
+                    currentState = State.Chase;
+
+                break;
+            case State.Chase:
+                currentTarget = player.transform;
+
+                currentTargetDirection = currentTarget.position - transform.position;
+                currentTargetDistance = Mathf.Sqrt(currentTargetDirection.x * currentTargetDirection.x + currentTargetDirection.y * currentTargetDirection.y + currentTargetDirection.z * currentTargetDirection.z);
+
+                if (currentTargetDistance < 2f)
+                {
+                    if (canAttack)
+                        Attack();
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentTarget.position.x, transform.position.y, currentTarget.position.z), moveSpeed * Time.deltaTime);
+                    transform.rotation = Quaternion.LookRotation(new Vector3(currentTargetDirection.x, 0, currentTargetDirection.z));
+                }
+
+                if (playerDistance > sightRange || playerAngle > oneSideFov)
+                    currentState = State.Roam;
+
+                break;
         }
+
+        //Debug.Log($"target: {currentTarget.name}, currDis: {currentTargetDistance}");
+        //Debug.Log($"state: {currentState}");
+    }
+
+    void Attack()
+    {
+        Debug.Log("Attacking");
+
+        player.TakeDamage(5);
+
+        StartCoroutine(Cooldown(1f));
+    }
+
+    IEnumerator Cooldown(float time)
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(time);
+        canAttack = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        Debug.Log($"{gameObject.name} took {damage} damage");
+
+        if (currentHP <= 0)
+            Debug.Log($"{gameObject.name} died");
     }
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int MaxHP;
+    [SerializeField] Enemy enemy;
+    [SerializeField] float radius = 2f;
+
+    bool canAttack;
+    int currentHP;
+
+    void Start()
+    {
+        canAttack = true;
+        currentHP = MaxHP;
+    }
+
+    void Update()
+    {
+        Vector3 enemyDir = enemy.transform.position - transform.position;
+        float distance = Mathf.Sqrt(enemyDir.x * enemyDir.x + enemyDir.y * enemyDir.y + enemyDir.z * enemyDir.z);
+        Vector3 enemyDirN = enemyDir / distance;
+        float dotForward = enemyDirN.x * transform.forward.x + enemyDirN.y * transform.forward.y + enemyDirN.z * transform.forward.z;
+        float dotRight = enemyDirN.x * transform.right.x + enemyDirN.y * transform.right.y + enemyDirN.z * transform.right.z;
+
+        float angle = Mathf.Acos(dotForward) * Mathf.Rad2Deg;
+        //Debug.Log($"Angle: {angle}");
+        //
+        //if (dotForward < 0 && dotRight < 0)
+        //    Debug.Log("Enemy on the back-left");
+        //else if (dotForward > 0 && dotRight < 0)
+        //    Debug.Log("Enemy on the front-left");
+        //else if (dotForward > 0 && dotRight > 0)
+        //    Debug.Log("Enemy on the front-right");
+        //else if (dotForward < 0 && dotRight > 0)
+        //    Debug.Log("Enemy on the back-right");
+        //
+        //if (distance <= radius + enemy.radius)
+        //    Debug.Log($"Collision detected");
+
+        if (canAttack && distance < 4f)
+            Attack();
+    }
+
+    void Attack()
+    {
+        Debug.Log("aewfaf");
+
+        enemy.TakeDamage(10);
+
+        StartCoroutine(Cooldown(1f));
+    }
+
+    IEnumerator Cooldown(float time)
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(time);
+        canAttack = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        Debug.Log($"{gameObject.name} took {damage} damage");
+
+        if (currentHP <= 0)
+        {
+            Debug.Log($"{gameObject.name} died");
+        }
+    }
+}
